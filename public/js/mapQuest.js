2bc7157e8f9ef13911df7667b737dac6f3372b6c
Eugina Palen <epalen@ucsd.edu>
2021-02-25 16:04:40 -0800

    map API and AJAX done

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'ktzGW2ZKDm03DOOZTfHHhld4D6qixN9I';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 15,
+		zoomControl: false
+	});
+
+	//add a marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
